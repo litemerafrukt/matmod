10e433391d81be615cbf52be5cd4e33bcd574cc2
Anders Nygren <litemerafrukt@gmail.com>
2017-12-04 13:10:26 +0100

    include repo link

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -4,5 +4,11 @@ export const Home = () => (
     <div>
         <h1>Hej!</h1>
         <p>Här presenterar jag mina kursmomentsrapporter för kursen "matmod" på BTH.</p>
+        <p>
+            Koden för sidan ligger på.{" "}
+            <a href="https://github.com/litemerafrukt/matmod">
+                https://github.com/litemerafrukt/matmod
+            </a>
+        </p>
     </div>
 );
